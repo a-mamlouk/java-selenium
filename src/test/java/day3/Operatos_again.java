753f15b4102f4a2dbab2da5e0599d27c3be2f531
Anas <anas.mamlouk@yahoo.fr>
2024-02-17 20:03:29 +0100

    Day 3 on Selenium with Pavan

--- src/test/java/day3/Operatos_again.java
+++ src/test/java/day3/Operatos_again.java
@@ -2,7 +2,7 @@ package day3;
 
 public class Operatos_again {
     public static void main(String args[]) {
-        //  Assignment ops
+    //  Assignment ops
         System.out.println("5-Testing Assignment Ops");
         int a=10;
         System.out.println(a);
@@ -19,9 +19,15 @@ public class Operatos_again {
         System.out.println(a);
 
     //  Condition ops
-    System.out.println("6-Testing Condition Ops");
-    int x=200,y=100;
-    int e= (x<y)? x: y; //if x>y => print x else print y
-    System.out.println(e);
+        System.out.println("6-Testing Condition Ops");
+        int x=200,y=100;
+        int e= (x<y)? x: y; //if x>y => print x else print y
+        System.out.println(e);
+
+        int person_age=30;
+        String res=(person_age>=18)? "Eligible": "Not Eligible";
+        System.out.println(res);
     }
+
+
 }
