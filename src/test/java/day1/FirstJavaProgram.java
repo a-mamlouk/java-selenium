659f36a16c7df0c143473597c533860d8c6d7b2d
Anas <anas.mamlouk@yahoo.fr>
2024-02-17 15:21:19 +0100

    Day 1 on Selenium with Pavan

---
+++ src/test/java/day1/FirstJavaProgram.java
@@ -0,0 +1,15 @@
+package day1;
+
+public class FirstJavaProgram {
+    public static void main(String args[])
+    {
+        System.out.println("welcome to java day1");
+        /*
+        System.out.println("this is a dummy comment");
+        System.out.println("this is a dummy comment");
+        System.out.println("this is a dummy comment");
+         */
+        System.out.println(20+10);
+        System.out.println("20+10");
+    }
+}
---
+++ src/test/java/day1/SecondJavaProgram.java
@@ -0,0 +1,10 @@
+package day1;
+
+public class SecondJavaProgram {
+    public static void main(String args[])
+    {
+        System.out.println("Again.. Welcome to java");
+        System.out.println("this is a test"); // commenting here
+        System.out.println("bullshit here"); //ctrl space didn't work on my IDE aqua maybe it works on eclipse using syso+ctrl+space
+    }
+}
