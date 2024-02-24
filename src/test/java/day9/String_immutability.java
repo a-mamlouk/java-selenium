93844f8ce3b44587c5c25194862d4294afc04c51
Anas <anas.mamlouk@yahoo.fr>
2024-02-24 13:46:56 +0100

    Day 9 on Selenium with Pavan

--- src/test/java/day9/String_immutability.java
+++ src/test/java/day9/String_immutability.java
@@ -2,7 +2,7 @@ package day9;
 
 public class String_immutability {
     public static void main(String[] args){
-        // first method
+        // Method : length(), charAT()
         String S="Welcome";
         String rev="";
         for (int i=S.length()-1; i>=0;i--){
@@ -11,5 +11,14 @@ public class String_immutability {
         }
         System.out.println("Reverse string is "+rev);
 
+        // Method 2:
+        String S2="Welcome";
+        String rev2= "";
+        char a[]= S2.toCharArray();
+        for (int i=a.length-1; i>=0; i--){
+            System.out.println(a[i]);
+            rev2=rev2+a[i];
+        }
+        System.out.println("Reverse string is "+rev2);
     }
 }
